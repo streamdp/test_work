364219b5f7cb2b967e3b67b96b57fb9ecd4560a6
Alexander Prymak <primak.aa@gmail.com>
2020-12-18 04:50:19 +0300

    Added assert in test

--- test_items.py
+++ test_items.py
@@ -3,5 +3,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_page_contains_an_add_to_cart_button(browser):
     browser.get(link)
-    browser.find_element_by_css_selector("button.btn-add-to-basket")
-
+    button = browser.find_element_by_css_selector("button.btn-add-to-basket")
+    assert button.is_enabled(), "Error, the button is inactive."
