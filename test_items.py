1e02f2e081bf43819ec8281bbd370b27bf9c9041
Alexander Prymak <primak.aa@gmail.com>
2020-12-18 04:52:35 +0300

    Added empty line to file

--- test_items.py
+++ test_items.py
@@ -5,3 +5,4 @@ def test_page_contains_an_add_to_cart_button(browser):
     browser.get(link)
     button = browser.find_element_by_css_selector("button.btn-add-to-basket")
     assert button.is_enabled(), "Error, the button is inactive."
+
